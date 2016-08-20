498215d872953c8e7364c0ad9a50c3f959cf1d0f
harryfeng <2307555175@qq.com>
2016-08-20 09:16:23 +0800

    add table view

--- iosKitchenSink/ViewController.m
+++ iosKitchenSink/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+@property (strong, nonatomic) UITableView *tableView;
+@property (strong, nonatomic) NSArray *tableCellTitle;
 
 @end
 
@@ -16,7 +18,65 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.tableView = [self makeTableView];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"newFriendCell"];
+    [self.view addSubview:self.tableView];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(UITableView *)makeTableView
+{
+    CGFloat x = 0;
+    CGFloat y = 50;
+    CGFloat width = self.view.frame.size.width;
+    CGFloat height = self.view.frame.size.height - 50;
+    CGRect tableFrame = CGRectMake(x, y, width, height);
+    
+    UITableView *tableView = [[UITableView alloc]initWithFrame:tableFrame style:UITableViewStylePlain];
+    
+    tableView.rowHeight = 45;
+    tableView.sectionFooterHeight = 22;
+    tableView.sectionHeaderHeight = 22;
+    tableView.scrollEnabled = YES;
+    tableView.showsVerticalScrollIndicator = YES;
+    tableView.userInteractionEnabled = YES;
+    tableView.bounces = YES;
+    
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    _tableCellTitle = [[NSArray alloc] initWithObjects:@"预留功能",@"预留功能",@"预留功能",@"预留功能",@"预留功能",@"预留功能", nil];
+    return tableView;
+}
+
+#pragma mark - UITableViewDataSource
+// number of section(s), now I assume there is only 1 section
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)theTableView
+{
+    return 1;
+}
+
+// number of row in the section, I assume there is only 1 row
+- (NSInteger)tableView:(UITableView *)theTableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"newFriendCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    };
+    cell.textLabel.text = _tableCellTitle[indexPath.row];
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
 }
 
 - (void)didReceiveMemoryWarning {
